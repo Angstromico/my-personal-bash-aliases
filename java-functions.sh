a839beb525ba5d4619db93842edce50c48272bf8
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-14 15:37:36 -0400

    feat: Make a Java file

--- java-functions.sh
+++ java-functions.sh
@@ -8,3 +8,23 @@ runjava() {
   # Run the compiled class
   java -cp src "$classname"
 }
+
+makejava() {
+  # Default class name is Main
+  local classname=${1:-Main}
+  local filepath="./src/${classname}.java"
+
+  cat > "$filepath" <<EOF
+public class ${classname} {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!\\n");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+EOF
+
+  echo "Created $filepath"
+}
