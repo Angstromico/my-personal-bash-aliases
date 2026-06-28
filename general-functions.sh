3a30137cb9d540b90c87cba31e078ed36c71a958
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-28 11:31:36 -0400

    feat(generals): addbash

--- general-functions.sh
+++ general-functions.sh
@@ -5,3 +5,20 @@ checkport() {
     fi
     sudo ss -tulpn | grep ":$1"
 }
+
+# Append code directly to .bashrc and reload the shell configuration
+addbash() {
+    if [ -z "$1" ]; then
+        echo "Error: You must provide the block of code or alias to add."
+        echo 'Example: addbash "alias ll='\''ls -la'\''"'
+        return 1
+    fi
+    
+    # Add line jump for clarity
+    echo "" >> ~/.bashrc
+    echo "$1" >> ~/.bashrc
+    
+    # Reload
+    source ~/.bashrc
+    echo "Successfully added to ~/.bashrc and reloaded configuration!"
+}
