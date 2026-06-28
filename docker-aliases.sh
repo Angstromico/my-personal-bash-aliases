4c44cbf03f2a7c8cc93572567c26f17a05781f66
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-28 11:19:49 -0400

    feat(docker): checkdockport

--- docker-aliases.sh
+++ docker-aliases.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 alias docker-start-img='docker run --rm -it -p'
+
+# Check which Docker container has mapped a specific port
+checkdockport() {
+    if [ -z "$1" ]; then
+        echo "Error: You must specify a port. Example: checkdockport 3000"
+        return 1
+    fi
+    docker ps | grep "$1"
+}
